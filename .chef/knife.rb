f7b34ebafc832f3fad666ad484f3f1ab6218cb34
HairyMezican <ryan.c.kohler@gmail.com>
2011-11-22 18:18:11 -0800

    validation stuff, and updated 'getting-started' stuff

---
+++ .chef/knife.rb
@@ -0,0 +1,14 @@
+current_dir = File.dirname(__FILE__)
+log_level                :info
+log_location             STDOUT
+node_name                "hairymezican"
+client_key               "#{current_dir}/hairymezican.pem"
+validation_client_name   "fastclash-validator"
+validation_key           "#{current_dir}/fastclash-validator.pem"
+chef_server_url          "https://api.opscode.com/organizations/fastclash"
+cache_type               'BasicFile'
+cache_options( :path => "#{ENV['HOME']}/.chef/checksums" )
+cookbook_path            ["#{current_dir}/../cookbooks"]
+
+knife[:aws_access_key_id] = "AKIAJA3LG65453DQIHRA"
+knife[:aws_secret_access_key] =  "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"
---
+++ client-config/client.rb
@@ -0,0 +1,4 @@
+log_level        :info
+log_location     STDOUT
+chef_server_url  'https://api.opscode.com/organizations/fastclash'
+validation_client_name 'fastclash-validator'
